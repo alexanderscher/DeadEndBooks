6e4257a525c6df50b83049f7b4150951a8bdb3a1
alexanderscher <alexcscher@gmail.com>
2023-10-10 19:06:35 -0700

    update

--- app/api/stripe/subscription-cancel/route.ts
+++ app/api/stripe/subscription-cancel/route.ts
@@ -1,15 +1,11 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-import Stripe from "stripe";
 import { authOptions } from "../../auth/[...nextauth]/route";
 import { ExtendedSession } from "@/types";
+import { stripe } from "@/stripe/stripe";
 
 export async function GET(req: NextRequest) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2023-08-16",
-  });
-
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
