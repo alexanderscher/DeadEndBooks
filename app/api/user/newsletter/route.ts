6aeea8549a6b974c6b61959f40094ec37f3f5bf6
alexanderscher <alexcscher@gmail.com>
2023-10-18 14:53:16 -0700

    update

--- app/api/user/newsletter/route.ts
+++ app/api/user/newsletter/route.ts
@@ -1,16 +1,14 @@
-import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-
 import prisma from "@/prisma/client";
 
-export async function PUT(request: Request) {
+export async function PUT(request: Request): Promise<NextResponse> {
   try {
     const json = await request.json();
     console.log(json);
     const userId = json.userId;
     const newsletter = json.newsletter;
 
-    const user = await prisma.user.update({
+    await prisma.user.update({
       where: {
         id: parseInt(userId),
       },
@@ -18,6 +16,8 @@ export async function PUT(request: Request) {
         newsletter: newsletter,
       },
     });
+
+    return new NextResponse("Success", { status: 200 }); // Return success response
   } catch (error: any) {
     return new NextResponse(error.message, {
       status: 500,
