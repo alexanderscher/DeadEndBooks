ff3c9952f05a6f302084b70b3c842db013e42271
alexanderscher <alexcscher@gmail.com>
2023-12-07 11:41:31 -0800

    update

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,29 +1,14 @@
 "use server";
 import { AdminNav, Navbar, Users } from "@/app/components";
-import { ExtendedSession } from "@/types";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/utils/auth";
-import { isProduction } from "@/utils/name";
 
 const page = async () => {
-  // const serverSession = await getServerSession(authOptions);
-  // const admin = (serverSession as ExtendedSession)?.user?.admin;
-
-  let data = null;
-
-  const url = isProduction();
-  const res = await fetch(`${url}/api/user`, {
-    next: { tags: ["users"], revalidate: 3600 },
-  });
-  data = await res.json();
-
   return (
     <main className={"page"}>
       <>
         <Navbar />
         <div className={" w-full"}>
           <AdminNav />
-          <Users res={data} />
+          <Users />
         </div>
       </>
     </main>
